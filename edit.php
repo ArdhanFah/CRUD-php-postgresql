631c3cb768a2a074856d289aa1699f1592fb637a
ArdhanFah <101484883+ArdhanFah@users.noreply.github.com>
2022-04-19 11:18:36 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -6,3 +6,28 @@ $query->execute();
 $data = $query->fetch();
 ?>
 
+<html>
+  <head>
+    <title>EDIT DATA</title>
+    <meta name="viewport" content="with-device-width, initial-scale=1.0">
+  </head>
+  <body>
+    <h2>ADD DATA SISWA</h2>
+    <form action="saveedit.php" method="POST">
+      <label>Nomor Induk Siswa</label>
+      <input type="text" name="nis" placeholder="Masukkan Nomor Induk Siswa" value="<?php echo $data['nis']?>" required>
+      <label>Nama Siswa</label>
+      <input type="text" name="nama" placeholder="Masukkan Nama Lengkap" value="<?php echo $data['nama']?>" required>
+      <label>Nomor Induk Siswa</label>
+      <select name="kelas">
+        <option></option>
+        <option value="TKJ">Teknik Komputer dan Jaringan</option>
+        <option value="RPL">Rekayasa Perangkat Lunak</option>
+        <option value="SIJA">System Informatika Jaringan dan Aplikasi</option>
+      </select>
+      <label>Alamat</label>
+      <input type="text" name="alamat" placeholder="Masukkan Alamat Lengkap" value="<?php echo $data['alamat']?>" required>
+      <button name="kirim" value="kirim">Simpan</button>
+    </form>
+  </body>
+</html>
