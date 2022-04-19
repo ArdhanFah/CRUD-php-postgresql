4a42a1b4e501e28924663b41df4d6bca47417113
ArdhanFah <101484883+ArdhanFah@users.noreply.github.com>
2022-04-19 11:26:56 +0700

    Update saveedit.php

--- saveedit.php
+++ saveedit.php
@@ -1,10 +1,10 @@
 <?php 
 include('config.php)';
 $nis = $_POST['nis]';
-$nis = $_POST['nama]';
-$nis = $_POST['jurusan]';
-$nis = $_POST['alamat]';
-$query = "UPDATE tbl_test SET nis='$nis',nama='$nama',jurusan='$jurusan',alamat='$alamat' WHERE nis='nis'";
+$nama = $_POST['nama]';
+$kelas = $_POST['kelas]';
+$alamat = $_POST['alamat]';
+$query = "UPDATE tbl_test SET nis='$nis',nama='$nama',kelas='$kelas',alamat='$alamat' WHERE nis='nis'";
 $connect->exec($query);
 header("location: index.php");
 ?>
