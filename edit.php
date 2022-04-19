ea800d998784fd216a5914d02c4f93d0be80dac7
ArdhanFah <101484883+ArdhanFah@users.noreply.github.com>
2022-04-19 11:19:28 +0700

    Update edit.php

--- edit.php
+++ edit.php
@@ -1,7 +1,7 @@
 <?php 
 include("config.php");
 $nis = $_GET['nis'];
-$query=$db->query("SELECT * FROM tbl_data WHERE nis='$nis'");
+$query=$connects->query("SELECT * FROM tbl_data WHERE nis='$nis'");
 $query->execute();
 $data = $query->fetch();
 ?>
